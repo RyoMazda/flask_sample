7f09c48d8aafd4491e60b701c1637d404c341f7f
RyoMatsuda@RCO <ryo_matsuda@r.recruit.co.jp>
2018-09-28 16:13:25 +0900

    add rds parameter group

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -12,9 +12,64 @@ resource "aws_db_subnet_group" "db_subnet_group" {
   }
 }
 
+resource "aws_rds_cluster_parameter_group" "rds_cluster_pg" {
+  name        = "fs-rds-cluster-pg"
+  family      = "aurora-mysql5.7"
+  description = "flask sample RDS cluster parameter group"
+
+  parameter {
+    name  = "character_set_server"
+    value = "utf8mb4"
+  }
+
+  parameter {
+    name  = "character_set_client"
+    value = "utf8mb4"
+  }
+
+  parameter {
+    name  = "character_set_connection"
+    value = "utf8mb4"
+  }
+
+  parameter {
+    name  = "character_set_database"
+    value = "utf8mb4"
+  }
+
+  parameter {
+    name  = "character_set_filesystem"
+    value = "utf8mb4"
+  }
+
+  parameter {
+    name  = "character_set_results"
+    value = "utf8mb4"
+  }
+
+  parameter {
+    name  = "collation_connection"
+    value = "utf8mb4_general_ci"
+  }
+
+  parameter {
+    name  = "collation_server"
+    value = "utf8mb4_general_ci"
+  }
+
+  parameter {
+    name  = "time_zone"
+    value = "Asia/Tokyo"
+  }
+
+  tags {
+    "Name"    = "flask-sample-db-cluster-pg"
+  }
+}
+
 resource "aws_rds_cluster" "db_cluster" {
   cluster_identifier           = "fs-db-cluster"
-  engine                       = "aurora-mysql"
+  engine                       = "aurora-mysql"  // Aurora (MySQL 5.7)
   database_name                = "${var.db_name}"
   master_username              = "${var.rds_master_username}"
   master_password              = "${var.rds_master_password}"
@@ -22,6 +77,7 @@ resource "aws_rds_cluster" "db_cluster" {
   preferred_backup_window      = "02:00-03:00"
   preferred_maintenance_window = "wed:03:00-wed:04:00"
   db_subnet_group_name         = "${aws_db_subnet_group.db_subnet_group.name}"
+  db_cluster_parameter_group_name = "${aws_rds_cluster_parameter_group.rds_cluster_pg.id}"
   final_snapshot_identifier    = "fs-db-cluster-snapshot"
 
   vpc_security_group_ids = ["${aws_security_group.rds-sg.id}"]
